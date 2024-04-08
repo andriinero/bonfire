02e4fe03ab6c4d25a0abfa221b4b232ddb4cd222
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-04-08 22:36:14 +0200

    style: chat room header

--- src/features/chat/components/ChatHeader.tsx
+++ src/features/chat/components/ChatHeader.tsx
@@ -1,5 +1,40 @@
+import { useAppSelector } from '@/app/hooks';
+
+import { selectSelectedChatId } from '../chatSlice';
+import ChatTitle from '@/features/chats/components/ChatTitle';
+import {
+  selectChatById,
+  selectParticipantsListByChatId,
+} from '@/features/chats/chatsSlice';
+import UserIcon from '@/components/general/UserIcon';
+import IconButton from '@/components/general/IconButton';
+import { FaEllipsis } from 'react-icons/fa6';
+
 const ChatHeader = () => {
-  return <header className="border-b p-4 shadow-[0_2px_4px_-2px_rgb(0,0,0,0.1)]">Header</header>;
+  const chatId = useAppSelector(selectSelectedChatId);
+  const chat = useAppSelector(selectChatById(chatId!));
+
+  const participant = chat?.participants[0];
+
+  return (
+    <header className="flex items-center justify-between border-b p-4 shadow-[0_2px_4px_-2px_rgb(0,0,0,0.1)]">
+      <div className="flex items-center gap-2">
+        <UserIcon
+          isOnline={participant?.is_online}
+          src={chat?.participants[0].profile_image}
+        />
+        <div>
+          <ChatTitle participants={chat?.participants!} />
+          <p className="text-sm text-neutral-500">
+            {participant?.is_online ? 'Online' : 'Offline'}
+          </p>
+        </div>
+      </div>
+      <IconButton className="text-sky-500">
+        <FaEllipsis />
+      </IconButton>
+    </header>
+  );
 };
 
 export default ChatHeader;
