a083d5403d3cfc5b4d93469f90b711a402bb16d7
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-06-03 23:37:42 +0200

    feat: enable vercel analytics

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -4,6 +4,7 @@ import Paths from '@/constants/Paths';
 
 import { selectIsSignedIn } from '@/features/auth/authSlice';
 
+import { Analytics } from '@vercel/analytics/react';
 import AppLogo from '@/components/general/AppLogo';
 import SignInPanel from '@/features/auth/components/SignInPanel';
 import PushNotificationList from '@/features/pushNotifications/components/PushNotificationList';
@@ -16,6 +17,7 @@ const SignIn = () => {
 
   return (
     <div className="flex min-h-dvh items-center justify-center bg-gray-100">
+      <Analytics />
       <main className="w-full max-w-lg space-y-8">
         <AppLogo />
         <h1 className="text-center text-2xl font-bold tracking-tight text-gray-900">
