819c6fe4ebdfe2471de3967be7034c435a8cd4a3
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-08-09 19:22:12 +0200

    style: notification item size to contain content

--- src/features/pushNotifications/components/PushNotificationList.tsx
+++ src/features/pushNotifications/components/PushNotificationList.tsx
@@ -8,7 +8,7 @@ const PushNotificationList = () => {
   const list = useAppSelector(selectPushNotificationsList);
 
   return (
-    <ul className="fixed right-0 top-0 z-10 w-full space-y-4 p-4 sm:w-auto sm:max-w-md">
+    <ul className="fixed right-0 top-0 z-10 flex flex-col items-end space-y-4 p-4">
       {list.map((n) => (
         <PushNotificationItem key={n._id} id={n._id} />
       ))}
