611176f74b68de0eb9f2ceac5f2629c9010f25f8
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-10-26 14:20:14 +0200

    refactor: add notification body style map

--- src/features/pushNotifications/components/NotificationBody.tsx
+++ src/features/pushNotifications/components/NotificationBody.tsx
@@ -2,6 +2,21 @@ import cn from '@/utils/cn';
 
 import { PushNotificationType } from '@/types/PushNotification';
 
+const styleMap: Record<
+  PushNotificationType,
+  { color: { main: string; list: string } }
+> = {
+  [PushNotificationType.ERROR]: {
+    color: { main: 'text-red-800', list: 'text-red-700' },
+  },
+  [PushNotificationType.SUCCESS]: {
+    color: { main: 'text-green-800', list: 'text-green-700' },
+  },
+  [PushNotificationType.WARNING]: {
+    color: { main: 'text-yellow-800', list: 'text-yellow-700' },
+  },
+};
+
 type NotificationBodyProps = {
   type: PushNotificationType;
   body?: string;
@@ -10,25 +25,12 @@ type NotificationBodyProps = {
 
 const NotificationBody = ({ type, body, list }: NotificationBodyProps) => {
   return (
-    <div
-      className={cn('text-medium space-y-0.5', {
-        'text-red-800': type === PushNotificationType.ERROR,
-        'text-green-800': type === PushNotificationType.SUCCESS,
-        'text-yellow-800': type === PushNotificationType.WARNING,
-      })}
-    >
+    <div className={cn('text-medium space-y-0.5', styleMap[type].color.main)}>
       {body ? <p>{body}</p> : <p>Internal Server Error 500</p>}
       {list ? (
         <ul className="list-disc pl-6 font-normal">
           {list.map((errorMessage, index) => (
-            <li
-              key={index}
-              className={cn({
-                'text-red-700': type === PushNotificationType.ERROR,
-                'text-green-700': type === PushNotificationType.SUCCESS,
-                'text-yellow-700': type === PushNotificationType.WARNING,
-              })}
-            >
+            <li key={index} className={cn('', styleMap[type].color.list)}>
               {errorMessage}
             </li>
           ))}
