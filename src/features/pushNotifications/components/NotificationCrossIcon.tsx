1dc7c00f95980573b52451c26634eefee98757e5
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-10-26 14:25:18 +0200

    refactor: add cross style map

--- src/features/pushNotifications/components/NotificationCrossIcon.tsx
+++ src/features/pushNotifications/components/NotificationCrossIcon.tsx
@@ -6,6 +6,12 @@ import type { MouseEventHandler } from 'react';
 import IconButton from '@/components/general/IconButton';
 import XIcon from '@/components/general/XIcon';
 
+const styleMap: Record<PushNotificationType, { color: string }> = {
+  [PushNotificationType.ERROR]: { color: 'text-red-400 hover:bg-red-100' },
+  [PushNotificationType.SUCCESS]: { color: 'text-red-400 hover:bg-green-100' },
+  [PushNotificationType.WARNING]: { color: 'text-red-400 hover:bg-yellow-100' },
+};
+
 type NotificationCrossIconProps = {
   type: PushNotificationType;
   onCrossClick: MouseEventHandler;
@@ -20,13 +26,7 @@ const NotificationCrossIcon = ({
   return (
     <IconButton className="self-start justify-self-end p-0.5">
       <XIcon
-        className={cn('text-sm', className, {
-          'text-red-400 hover:bg-red-100': type === PushNotificationType.ERROR,
-          'text-green-400 hover:bg-green-100':
-            type === PushNotificationType.SUCCESS,
-          'text-yellow-400 hover:bg-yellow-100':
-            type === PushNotificationType.WARNING,
-        })}
+        className={cn('rounded text-sm', styleMap[type].color, className)}
         onClick={onCrossClick}
       />
     </IconButton>
