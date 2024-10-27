d21382f4dfd51f5f86970c33ab4a63144b501cbf
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-10-27 16:35:21 +0100

    style: adjust messages content colors

--- src/features/messages/components/ActionMessage.tsx
+++ src/features/messages/components/ActionMessage.tsx
@@ -3,7 +3,7 @@ type ActionMessageProps = { body: string };
 const ActionMessage = ({ body }: ActionMessageProps) => {
   return (
     <li aria-label="chat-message" className="flex justify-center text-sm">
-      <p className="rounded-full bg-gray-100 px-2 py-1 text-xs text-gray-600">
+      <p className="rounded-full bg-gray-100 px-3 py-1 text-xs text-gray-700">
         {body}
       </p>
     </li>
