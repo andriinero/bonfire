4e8f543ec94ca4c5795ddfdbfbe34911fbc91e1c
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-11-05 18:39:03 +0100

    feat: contact multi select field

--- src/components/general/MultiSelect.tsx
+++ src/components/general/MultiSelect.tsx
@@ -1,23 +1,46 @@
+import { useRef, useState } from 'react';
+
+import type { User } from '@/types/User';
 import type { ChangeEvent } from 'react';
 
-import ContactsItem from '@/features/contacts/components/ContactsItem';
+import ContactSearchItem from '@/features/contacts/components/ContactSearchItem';
 import { X } from 'lucide-react';
 import TextInput from '../form/TextInput';
 import IconButton from './IconButton';
 
 type MultiSelectProps = {
-  onCloseClick?: () => void;
+  onCloseClick: () => void;
 };
 
-let timeout: NodeJS.Timeout;
 const REQUEST_DELAY_MS = 500;
 
+const joe = {
+  _id: '1',
+  username: 'TheAverageJoe',
+  email: 'joe',
+  role: 'user' as const,
+  created: '',
+  is_online: true,
+  profile_image: '',
+  color_class: 'amber-400',
+};
+
 const MultiSelect = ({ onCloseClick }: MultiSelectProps) => {
+  const [contacts] = useState<User[]>([joe]);
+  const [selectedItems, setSelectedItems] = useState<User[]>([]);
+  const timeoutRef = useRef<NodeJS.Timeout>();
+
+  const handleCloseForm = () => {
+    onCloseClick();
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+  };
+
   const handleTextInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (timeout) clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      // FIXME: remove comment
-      console.log(e.target.value);
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => {
+      if (e.target.value)
+        // FIXME: remove comment
+        console.log(e.target.value);
     }, REQUEST_DELAY_MS);
   };
 
@@ -30,15 +53,33 @@ const MultiSelect = ({ onCloseClick }: MultiSelectProps) => {
           className="rounded-xl"
         />
         <div className="animate-in fade-in slide-in-from-top-2 zoom-in-95 absolute z-10 mt-2 flex w-full flex-col gap-2 rounded-xl bg-white p-3 shadow-md ring-1 ring-gray-300">
-          {/* @ts-expect-error this is an example */}
-          <ContactsItem />
-          {/* @ts-expect-error this is an example */}
-          <ContactsItem />
-          {/* @ts-expect-error this is an example */}
-          <ContactsItem />
+          <div className="flex basis-40 flex-col justify-center">
+            {contacts.length !== 0 ? (
+              <ul className="mb-auto">
+                {contacts.map((contact) => {
+                  const handleContactAdd = () => {
+                    setSelectedItems([...selectedItems, contact]);
+                    // FIXME: remove comment
+                    console.log(selectedItems);
+                  };
+                  return (
+                    <ContactSearchItem
+                      key={contact._id}
+                      handleContactAdd={handleContactAdd}
+                      {...contact}
+                    />
+                  );
+                })}
+              </ul>
+            ) : (
+              <h1 className="text-center text-sm">
+                The search has returned no results
+              </h1>
+            )}
+          </div>
         </div>
       </div>
-      <IconButton className="p-2" onClick={onCloseClick}>
+      <IconButton className="p-2" onClick={handleCloseForm}>
         <X />
       </IconButton>
     </div>
