e01a681b8ac2a0e1ba098eb1843964e40ce722c8
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2024-11-15 19:51:23 +0100

    feat: add recommended contact on click

--- src/features/contacts/components/RecommendedContactItem.tsx
+++ src/features/contacts/components/RecommendedContactItem.tsx
@@ -1,20 +1,35 @@
 import { useAppSelector } from '@/app/hooks';
-import { selectRecommendedContactById } from '../contactsSlice';
+
+import {
+  contactsApiSlice,
+  selectRecommendedContactById,
+  usePostContactMutation,
+} from '../contactsSlice';
+
 import UserIcon from '@/components/general/UserIcon';
 
 type RecommendedContactItemProps = { contactId: string };
 
 const RecommendedContactItem = ({ contactId }: RecommendedContactItemProps) => {
-  const contact = useAppSelector(selectRecommendedContactById(contactId));
+  const contact = useAppSelector(selectRecommendedContactById(contactId))!;
+
+  const [postContact] = usePostContactMutation();
+
+  const handleContactClick = async (): Promise<void> => {
+    postContact({ contactUsername: contact.username });
+  };
 
   return (
-    <li className="flex flex-col items-center gap-1">
+    <li
+      onClick={handleContactClick}
+      className="flex cursor-pointer flex-col items-center gap-1"
+    >
       <UserIcon
-        title={contact?.username}
-        colorClass={contact?.colorClass}
-        src={contact?.profileImage}
+        title={contact.username}
+        colorClass={contact.colorClass}
+        src={contact.profileImage}
       />
-      <p className="text-sm font-medium">{contact?.username}</p>
+      <p className="text-sm font-medium">{contact.username}</p>
     </li>
   );
 };
