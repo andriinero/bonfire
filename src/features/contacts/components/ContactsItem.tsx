d40dc01f8988a5a12825d638008289f8a09054fe
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2025-01-05 20:53:18 +0100

    style: contact item hover bg color

--- src/features/contacts/components/ContactsItem.tsx
+++ src/features/contacts/components/ContactsItem.tsx
@@ -4,16 +4,19 @@ import { motion } from 'framer-motion';
 import {
   selectContactById,
   selectedContactIdSet,
+  selectSelectedContactId,
   useDeleteContactMutation,
 } from '../contactsSlice';
 
 import UserIcon from '@/components/general/UserIcon';
 import { Button } from '@/components/ui/button';
 import { UserRoundMinus } from 'lucide-react';
+import cn from '@/utils/cn';
 
 type ContactsItemProps = { contactId: string };
 
 const ContactsItem = ({ contactId }: ContactsItemProps) => {
+  const selectedContactId = useAppSelector(selectSelectedContactId);
   const contact = useAppSelector(selectContactById(contactId))!;
   const [deleteContact] = useDeleteContactMutation();
   const dispatch = useAppDispatch();
@@ -29,10 +32,15 @@ const ContactsItem = ({ contactId }: ContactsItemProps) => {
     });
   };
 
+  const isSelectedContact = contactId === selectedContactId;
+
   return (
     <motion.li
       key={contact?.id}
-      className="flex items-center gap-4 p-2 hover:cursor-pointer hover:bg-gray-100"
+      className={cn(
+        'flex items-center gap-4 rounded-lg p-2 hover:cursor-pointer hover:bg-gray-50',
+        { 'bg-gray-100': isSelectedContact },
+      )}
     >
       <UserIcon
         onClick={handleSetSelectedContactId}
